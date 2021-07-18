cba9d248d2cce6dfe6838dbd81a69a5d733de09d
123wuyu <350197427@qq.com>
2021-07-18 10:49:35 +0800

    '0718'

--- flask_code/day1/news/views.py
+++
@@ -1,6 +0,0 @@
-from . import news_bp
-
-# 2.定义视图函数和路由
-@news_bp.route("/news")
-def get_news():
-    return 'news info'
--- flask_code/day1/set.py
+++
@@ -1,9 +0,0 @@
-class DefaultConfig(object):
-    MYSQL_URL = 'mysql address'
-    REDIS_URL = 'redis address'
-
-# 定义其它模式下的配置信息，生产模式、开发模式
-class ProductionConfig(DefaultConfig):
-    MYSQL_URL = 'production mysql address'
-    REDIS_URL = 'production redis address'
-
